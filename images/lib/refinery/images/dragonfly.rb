386d34387bb1b83cfb068cca1a365684ce895286
Abelardo <abelardogilm@gmail.com>
2013-09-09 12:20:55 +0200

    showing datastore on staging

--- images/lib/refinery/images/dragonfly.rb
+++ images/lib/refinery/images/dragonfly.rb
@@ -19,28 +19,28 @@ module Refinery
 
           app_images = ::Dragonfly[:refinery_images]
 
-          #app_images.configure do |config|
-          #  config.server.before_serve do |job, env|
-          #    uid = job.store
-          #    # Keep track of its uid
-          #    # Holds all the job info, e.g fetch 'image_uid' then resize to '40x40'
-          #    Refinery::Thumb.create!( :uid => uid, :job => job.serialize )
-          #
-          #  end
-            #
-            ## Next we define the url for our processed images, overriding the default .url method...
-            #config.define_url do |app, job, opts|
-            #  thumb = Refinery::Thumb.find_by_job(job.serialize)
-            #  # If (the job fetch 'image_uid' then resize to '40x40') has been stored already..
-            #  # then serve the url from the datastore filesystem, s3, etc
-            #  if thumb
-            #    app.datastore.url_for(thumb.uid)
-            #  else
-            #    # ...otherwise if the job hasn't been stored, serve it from the Dragonfly server as usual
-            #    app.server.url_for(job)
-            #  end
-            #end
-  #        end
+          app_images.configure do |config|
+            config.server.before_serve do |job, env|
+              uid = job.store
+              # Keep track of its uid
+              # Holds all the job info, e.g fetch 'image_uid' then resize to '40x40'
+              Refinery::Thumb.create!( :uid => uid, :job => job.serialize )
+
+            end
+
+            # Next we define the url for our processed images, overriding the default .url method...
+            config.define_url do |app, job, opts|
+              thumb = Refinery::Thumb.find_by_job(job.serialize)
+              # If (the job fetch 'image_uid' then resize to '40x40') has been stored already..
+              # then serve the url from the datastore filesystem, s3, etc
+              if thumb
+                app.datastore.url_for(thumb.uid)
+              else
+                # ...otherwise if the job hasn't been stored, serve it from the Dragonfly server as usual
+                app.server.url_for(job)
+              end
+            end
+          end
 
 
           app_images.configure_with(:rails) do |c|
